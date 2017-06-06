388f85ecd634a88c0b17d07718932eaeb4477561
Ubuntu <ubuntu@localhost.dev>
2017-06-06 01:17:13 -0600

    Added 2.8.2 compatibility

--- index.php
+++ index.php
@@ -20,6 +20,10 @@ if (isset($_GET['q'])) {
 			if (file_exists($path.'/app/version.txt')) {
 				$file = fopen($path.'/app/version.txt', 'r') or die('Unable to open file!');
 				$version = fread($file , filesize($path.'/app/version.txt'));
+				if (strpos($version, '-dev') !== false) {
+					$version = explode('-', $version);
+					$version = $version[0];
+				}
 				echo $whitelabeler->templateVersions($version);
 			} else {
 				echo 0;
