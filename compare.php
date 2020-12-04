c60e32ae412defa19f9bb8de7ebf4fac9a0209d9
Nick Thompson <nick@cognize.co>
2020-12-03 23:36:48 -0700

    Add 3.2.0-3.2.1 Support

--- compare.php
+++ compare.php
@@ -5,8 +5,8 @@
 require_once('whitelabeler.php');
 $whitelabeler = new Whitelabeler;
 
-$v1_path = '../3.1.1';
-$v2_path = '../3.1.2';
+$v1_path = '../3.2.0';
+$v2_path = '../3.2.1';
 
 $compare = $whitelabeler->compareMauticVersions($v1_path, $v2_path);
 
