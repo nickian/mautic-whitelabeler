787eba623e3743463449b04b175c2b27dfc0bb2d
Nick Thompson <nick@cognize.co>
2021-03-03 12:49:36 -0700

    3.3.1 Support

--- compare.php
+++ compare.php
@@ -5,8 +5,8 @@
 require_once('whitelabeler.php');
 $whitelabeler = new Whitelabeler;
 
-$v1_path = '../3.2.4';
-$v2_path = '../3.2.5';
+$v1_path = '../3.2.5';
+$v2_path = '../3.3.1';
 
 $compare = $whitelabeler->compareMauticVersions($v1_path, $v2_path);
 
