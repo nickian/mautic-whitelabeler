458e692604066467168e447c8cf552b9f3e27cea
Nick Thompson <nick@nick.do>
2021-03-30 18:59:10 +0000

    3.3.2 Support

--- compare.php
+++ compare.php
@@ -5,8 +5,8 @@
 require_once('whitelabeler.php');
 $whitelabeler = new Whitelabeler;
 
-$v1_path = '../3.2.5';
-$v2_path = '../3.3.1';
+$v1_path = '../3.3.1';
+$v2_path = '../3.3.2';
 
 $compare = $whitelabeler->compareMauticVersions($v1_path, $v2_path);
 
