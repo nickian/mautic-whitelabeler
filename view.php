298f9d12441db593f9ac89c6f0b999a08350c35a
Ivan Klymenchenko <developer@iillexial.me>
2022-03-06 18:18:27 +0400

    Make default values for fields from env variables

--- view.php
+++ view.php
@@ -463,7 +463,35 @@
 		var mautic_path = '<?=realpath(__DIR__ . '/..');?>';
 	</script>
 	<style>
-	</style>
+    </style>
+    <script>
+        var wl = mautic_url.split('/')[mautic_url.split('/').length-1];
+        var mautic_url = "<?=getenv('MAUTIC_URL')?>";
+        var company_name = "<?=getenv('COMPANY_NAME')?>";
+        var footer_prefix = "<?=getenv('FOOTER_PREFIX')?>";
+        var mautic_primary = "<?=getenv('PRIMARY_COLOR') ?? '#4e5d9d'?>";
+        var mautic_hover = "<?=getenv('HOVER_COLOR') ?? '#3d497b'?>";
+        var logo_bg = "<?=getenv('LOGO_BACKGROUND') ?? '#4e5d9d'?>";
+        var sidebar_bg = "<?=getenv('SIDEBAR_BACKGROUND') ?? '#1d232b'?>";
+        var sidebar_submenu_bg = "<?=getenv('SUBMENU_BACKGROUND') ?? '#171c22'?>";
+        var sidebar_link = "<?=getenv('LINK_COLOR') ?? '#9e9e9e'?>";
+        var sidebar_link_hover = "<?=getenv('LINK_HOVER_COLOR') ?? '#ffffff'?>";
+        var active_icon = "<?=getenv('ACTIVE_ICON_COLOR') ?? '#fdb933'?>";
+        var sidebar_divider = "<?=getenv('DIVIDER_COLOR') '#202830'?>";
+        var submenu_bullet_bg = "<?=getenv('SUBMENU_BULLET_BACKGROUND') ?? '#222a32'?>";
+        var submenu_bullet_shadow = "<?=getenv('SUBMENU_BULLET_SHADOW') ?? '#1a2026'?>";
+        var divider_left = <?=getenv('DIVIDER_LEFT_POSITION') ?? 50?>;
+        var sidebar_logo = null;
+        var sidebar_logo_width = <?=getenv('SIDEBAR_LOGO_WIDTH' ?? 130?>;
+        var sidebar_logo_margin_top = <?=getenv('SIDEBAR_LOGO_TOP_MARGIN') ?? 10?>;
+        var sidebar_logo_margin_left = <?=getenv('SIDEBAR_LOGO_LEFT_MARGIN') ?? 0?>;
+        var sidebar_logo_margin_right = <?=getenv('SIDEBAR_LOGO_RIGHT_MARGIN') ?? 0?>;
+        var login_logo_width = <?=getenv('LOGIN_LOGO_WIDTH') ?? 100?>;
+        var login_logo_margin_top = <?=getenv('LOGIN_LOGO_TOP_MARGIN') ?? 20?>;
+        var login_logo_margin_bottom = <?=getenv('LOGIN_LOGO_BOTTOM_MARGIN') ?? 20?>;
+        var login_logo = null;
+        var favicon = null;
+    </script>
 	<script src="lib/whitelabeler.js"></script>
 </body>
 </html>
