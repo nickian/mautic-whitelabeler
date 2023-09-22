cb5a0d09561b078c6dd181b9779527ac7f7241d7
Nick Thompson <nick@nick.do>
2023-09-22 14:19:36 -0600

    4.4.10 support

--- compare.php
+++ compare.php
@@ -5,8 +5,8 @@
 require_once('whitelabeler.php');
 $whitelabeler = new Whitelabeler;
 
-$v1_path = '4.4.8';
-$v2_path = '4.4.9';
+$v1_path = '4.4.9';
+$v2_path = '4.4.10';
 
 $compare = $whitelabeler->compareMauticVersions($v1_path, $v2_path);
 
